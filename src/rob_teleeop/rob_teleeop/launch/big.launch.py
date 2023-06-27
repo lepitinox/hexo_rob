5e4ee3207a1f23b44b364318b81a83cf8af4b6e7
Adrien <adrienducourthial@gmail.com>
2023-06-27 17:25:09 +0200

    ssd

--- src/rob_teleeop/rob_teleeop/launch/big.launch.py
+++ src/rob_teleeop/rob_teleeop/launch/big.launch.py
@@ -1,17 +1,12 @@
 import os
 
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFile, LaunchConfiguration
 from launch_ros.actions import Node
+from pathlib import Path
 
 def generate_launch_description():
 
-    # Get the file path to the example RViz configuration and the example URDF file
-    rviz_config_file = os.path.join(get_package_share_directory('your_package_name'), 'config', 'example.rviz')
-    urdf_file = os.path.join(get_package_share_directory('your_package_name'), 'urdf', 'base.urdf.xacro')
     
     # Configure the RViz2 node
     rviz_node = Node(
@@ -19,7 +14,6 @@ def generate_launch_description():
         node_executable='rviz2',
         node_name='rviz2',
         output='both',
-        arguments=['-d', rviz_config_file]
     )
 
     # Configure the robot_state_publisher node
@@ -28,15 +22,11 @@ def generate_launch_description():
         node_executable='robot_state_publisher',
         node_name='robot_state_publisher',
         output='both',
-        parameters=[{'robot_description': urdf_file}],
+        parameters=[{'robot_description': "/home/ubuntu/rob/src/rob_teleeop/rob_teleeop/urdf/robot.urdf"}],
     )
 
     # Describe the launch process
     ld = LaunchDescription([
-        DeclareLaunchArgument(
-            'rviz_config_file',
-            default_value=rviz_config_file,
-            description='Full path to the RVIZ configuration file'),
         rviz_node,
         robot_state_publisher,
     ])
