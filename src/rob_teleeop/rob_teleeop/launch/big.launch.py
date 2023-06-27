6313caa063a1382be1b509904438354e066e9c80
Adrien <adrienducourthial@gmail.com>
2023-06-27 17:26:12 +0200

    dqss

--- src/rob_teleeop/rob_teleeop/launch/big.launch.py
+++ src/rob_teleeop/rob_teleeop/launch/big.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
     # Configure the RViz2 node
     rviz_node = Node(
         package='rviz2',
-        node_executable='rviz2',
+        executable='rviz2',
         node_name='rviz2',
         output='both',
     )
@@ -19,7 +19,7 @@ def generate_launch_description():
     # Configure the robot_state_publisher node
     robot_state_publisher = Node(
         package='robot_state_publisher',
-        node_executable='robot_state_publisher',
+        executable='robot_state_publisher',
         node_name='robot_state_publisher',
         output='both',
         parameters=[{'robot_description': "/home/ubuntu/rob/src/rob_teleeop/rob_teleeop/urdf/robot.urdf"}],
