06465986e74609f5663ffc256f1a9c3bb4f79b9b
Adrien Ducourthial <adrienducourthial@gmail.com>
2023-06-27 22:59:00 +0200

    sd

--- src/rob_teleeop/rob_teleeop/launch/big.launch.py
+++ src/rob_teleeop/rob_teleeop/launch/big.launch.py
@@ -4,8 +4,10 @@ import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+
+from launch_ros.parameter_descriptions import ParameterValue
 
 from pathlib import Path
 
@@ -29,16 +31,21 @@ def generate_launch_description():
     path = "/home/ubuntu/hexo_rob/src/rob_teleeop/rob_teleeop/urdf/test.urdf"
     robot_description_content = Command([path])
 
+    robot_description_content = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
+                                       value_type=str)
+
     robot_description = {"robot_description": robot_description_content}
     with open(path, "r") as file:
         data = file.read()
+
+
     # Configure the robot_state_publisher node
     robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         name="robot_state_publisher",
         output="screen",
-        parameters=[{"robot_description": data}],
+        parameters=[{"robot_description": robot_description}],
     )
 
     # Describe the launch process
