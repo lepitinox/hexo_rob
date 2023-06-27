74ea221672a1384118607a57d4d2e0ec8fe886c4
Adrien <adrienducourthial@gmail.com>
2023-06-27 18:01:21 +0200

    qds

--- src/rob_teleeop/rob_teleeop/launch/big.launch.py
+++ src/rob_teleeop/rob_teleeop/launch/big.launch.py
@@ -3,6 +3,9 @@ import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable,PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
 from pathlib import Path
 
 def generate_launch_description():
@@ -15,13 +18,16 @@ def generate_launch_description():
         name='rviz2',
         arguments=['-d', '/home/ubuntu/rob/src/rob_teleeop/rob_teleeop/config/big.rviz'],
     )
-    
+    robot_description_content = Command([PathJoinSubstitution([FindExecutable(name="xacro")])," ",PathJoinSubstitution([FindPackageShare("rob_teleeop"),"rob_teleeop/urdf","my_robot.urdf"])])
+
+    robot_description = {"robot_description": robot_description_content}
 
     # Configure the robot_state_publisher node
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
+        output='screen',
         parameters=[{'robot_description': "/home/ubuntu/rob/src/rob_teleeop/rob_teleeop/urdf/robot.urdf"}],
     )
 
