52d607005c6d13644a7d5c38a28922954ad63d32
Adrien Ducourthial <adrienducourthial@gmail.com>
2023-06-27 21:14:53 +0200

    sqddd

--- src/rob_teleeop/rob_teleeop/launch/big.launch.py
+++ src/rob_teleeop/rob_teleeop/launch/big.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
    #    name='rviz2',
    #    arguments=['-d', '/home/ubuntu/rob/src/rob_teleeop/rob_teleeop/config/big.rviz'],
    #)
-    robot_description_content = Command([PathJoinSubstitution([FindPackageShare("rob_teleeop"),"rob_teleeop/urdf","base.urdf.xacro"])])
+    robot_description_content = Command([PathJoinSubstitution([FindPackageShare("rob_teleeop"),"base.urdf.xacro"])])
 
     robot_description = {"robot_description": robot_description_content}
 
