c5aed5bc1bf65dd9ecb8c68fd6c55a0ec785ec00
Adrien <adrienducourthial@gmail.com>
2023-06-28 09:19:06 +0200

    qsd

--- src/rob_teleeop/rob_teleeop/launch/big.launch.py
+++ src/rob_teleeop/rob_teleeop/launch/big.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
                                       description='Absolute path to robot urdf file')
     
     
-    robot_description = ParameterValue(Command([LaunchConfiguration('model')]),
+    robot_description = ParameterValue(Command(["xacro ",LaunchConfiguration('model')]),
                                        value_type=str)
 
 
