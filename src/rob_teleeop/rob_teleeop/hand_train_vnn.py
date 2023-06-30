1340946b9d5f73cfdb4706ea99eb90f0db08853e
Adrien Ducourthial <adrienducourthial@gmail.com>
2023-06-30 11:54:20 +0200

    qsd

--- src/rob_teleeop/rob_teleeop/hand_train_vnn.py
+++ src/rob_teleeop/rob_teleeop/hand_train_vnn.py
@@ -29,14 +29,12 @@ input_shape = (28, 28, 1)
 
 model = keras.Sequential()
 model.add(layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-
 model.add(layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(layers.MaxPooling2D(pool_size=(2, 2)))
-
 model.add(layers.Flatten())
-model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(128, activation='softmax'))
 
 early_stop = EarlyStopping(monitor='val_loss',patience=2)
 
