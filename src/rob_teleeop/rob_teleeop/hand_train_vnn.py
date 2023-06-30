f79482a9b9a5de2f4731e1221eedc6ee8abff25e
Adrien Ducourthial <adrienducourthial@gmail.com>
2023-06-30 11:10:12 +0200

    qsd

--- src/rob_teleeop/rob_teleeop/hand_train_vnn.py
+++ src/rob_teleeop/rob_teleeop/hand_train_vnn.py
@@ -6,37 +6,46 @@ from tensorflow.keras import layers
 from tensorflow.keras.callbacks import EarlyStopping
 from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
+import rclpy
 
 
-share_path = Path(get_package_share_directory('rob_teleeop'))
-
-train_df = pd.read_csv(share_path/'sign_mnist_train.csv')
-test_df = pd.read_csv(share_path/'sign_mnist_test.csv')
 
 
-X_train = np.array(train_df.iloc[:, 1:]).reshape(-1, 28, 28, 1)
-y_train = np.array(train_df.iloc[:, 0])
+def main(args=None):
+    rclpy.init(args=args)
+    node = rclpy.create_node('hand_inf')
+    rclpy.spin(node)
+    rclpy.shutdown()
+    
+    share_path = Path(get_package_share_directory('rob_teleeop'))
 
-X_test = np.array(test_df.iloc[:, 1:]).reshape(-1, 28, 28, 1)
-y_test = np.array(test_df.iloc[:, 0])
+    train_df = pd.read_csv(share_path/'sign_mnist_train.csv')
+    test_df = pd.read_csv(share_path/'sign_mnist_test.csv')
 
-input_shape = (28, 28, 1)
 
-model = keras.Sequential()
-model.add(layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+    X_train = np.array(train_df.iloc[:, 1:]).reshape(-1, 28, 28, 1)
+    y_train = np.array(train_df.iloc[:, 0])
 
-model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    X_test = np.array(test_df.iloc[:, 1:]).reshape(-1, 28, 28, 1)
+    y_test = np.array(test_df.iloc[:, 0])
 
-model.add(layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    input_shape = (28, 28, 1)
 
-model.add(layers.Flatten())
-model.add(layers.Dense(128, activation='relu'))
+    model = keras.Sequential()
+    model.add(layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
 
-early_stop = EarlyStopping(monitor='val_loss',patience=2)
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.add(layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 
-model.fit(x=X_train, y=y_train, validation_data=(X_test, y_test),epochs=25, callbacks=[early_stop])
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
 
-model.save(share_path/'hand_sign_model.h5')
+    early_stop = EarlyStopping(monitor='val_loss',patience=2)
+
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    model.fit(x=X_train, y=y_train, validation_data=(X_test, y_test),epochs=25, callbacks=[early_stop])
+
+    model.save(share_path/'hand_sign_model.h5')
